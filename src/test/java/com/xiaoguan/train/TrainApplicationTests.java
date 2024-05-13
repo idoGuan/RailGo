51a7c0a8e7cf02e108f0dc39852a29fbcc2a4fad
ZWHY <1760785369@qq.com>
2024-05-13 21:34:28 +0800

    初始化

---
+++ src/test/java/com/xiaoguan/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoguan.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

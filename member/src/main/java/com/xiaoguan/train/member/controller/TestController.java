2ee6f590292e01a226122d21541d2a6bc1967897
ZWHY <1760785369@qq.com>
2024-05-14 13:54:55 +0800

    新增member会员模块

---
+++ member/src/main/java/com/xiaoguan/train/member/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.xiaoguan.train.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: TestController
+ * Package: com.xiaoguan.train.member.controller
+ * Description:
+ *
+ * @Author 小管不要跑
+ * @Create 2024/5/14 13:47
+ * @Version 1.0
+ */
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}

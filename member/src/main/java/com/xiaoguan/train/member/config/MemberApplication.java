9100c7771ac0d9f8415f652155f3b99b7fc91658
ZWHY <1760785369@qq.com>
2024-05-14 15:09:33 +0800

    增加http测试文件，方便直接在IDEA里测试

--- member/src/main/java/com/xiaoguan/train/member/config/MemberApplication.java
+++ member/src/main/java/com/xiaoguan/train/member/config/MemberApplication.java
@@ -18,7 +18,7 @@ public class MemberApplication {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 }

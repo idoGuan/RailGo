85a68a5bde672455f83d3ee0a39174ac93dc9547
ZWHY <1760785369@qq.com>
2024-05-19 16:19:10 +0800

    制作queryResp.ftl模板

--- generator/src/main/java/com/xiaoguan/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/xiaoguan/train/generator/server/ServerGenerator.java
@@ -81,10 +81,11 @@ public class ServerGenerator {
 
 
 
-        gen(Domain, param, "service", "service");
-        gen(Domain, param, "controller", "controller");
-        gen(Domain, param, "req", "saveReq");
+//        gen(Domain, param, "service", "service");
+//        gen(Domain, param, "controller", "controller");
+//        gen(Domain, param, "req", "saveReq");
         gen(Domain, param, "req", "queryReq");
+        gen(Domain, param, "resp", "queryResp");
 
     }
 
--- member/src/main/java/com/xiaoguan/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/xiaoguan/train/member/resp/PassengerQueryResp.java
@@ -1,28 +1,49 @@
 package com.xiaoguan.train.member.resp;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class PassengerQueryResp {
-    @JsonSerialize(using = ToStringSerializer.class)
+
+    /**
+     * id
+     */
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
 
-    @JsonSerialize(using = ToStringSerializer.class)
+    /**
+     * 会员id
+     */
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
+    /**
+     * 姓名
+     */
     private String name;
 
+    /**
+     * 身份证
+     */
     private String idCard;
 
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
     private String type;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GTM+8")
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GTM+8")
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {

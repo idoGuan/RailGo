1c4b93744a605a55c25e839e9d1dbfec38ef6c07
ZWHY <1760785369@qq.com>
2024-05-19 15:30:25 +0800

    增减DBUtil，获取表字段信息

--- generator/src/main/java/com/xiaoguan/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/xiaoguan/train/generator/server/ServerGenerator.java
@@ -1,6 +1,8 @@
 package com.xiaoguan.train.generator.server;
 
 
+import com.xiaoguan.train.generator.util.DbUtil;
+import com.xiaoguan.train.generator.util.Field;
 import com.xiaoguan.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -11,6 +13,7 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -42,6 +45,18 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
+
         // 示例：表名 jiawa_test
         // Domain = JiawaTest
         String Domain = domainObjectName.getText();
@@ -50,8 +65,8 @@ public class ServerGenerator {
         // do_main = jiawa-test
         String do_main = tableName.getText().replaceAll("_", "-");
         // 表中文名
-//        String tableNameCn = DbUtil.getTableComment(tableName.getText());
-//        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
 //        Set<String> typeSet = getJavaTypes(fieldList);
 
         // 组装参数
@@ -65,6 +80,7 @@ public class ServerGenerator {
 //        param.put("typeSet", typeSet);
 //        param.put("readOnly", readOnly);
         System.out.println("组装参数：" + param);
+
 
 
         gen(Domain, param, "service");

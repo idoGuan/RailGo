5441fa0cfae06e297965c8c8312bc7cdbfec6345
ZWHY <1760785369@qq.com>
2024-05-20 16:19:34 +0800

    增加business业务模块

---
+++ businesss/src/main/java/com/xiaoguan/train/business/config/BusinessApplication.java
@@ -0,0 +1,25 @@
+package com.xiaoguan.train.business.config;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.xiaoguan")
+@MapperScan("com.xiaoguan.train.member.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+}

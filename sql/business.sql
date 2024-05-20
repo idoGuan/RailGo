9c47cfe52f71a377d6c7ac865f47347a3bf64a71
ZWHY <1760785369@qq.com>
2024-05-20 18:15:59 +0800

    配置数据库增加business库，配置持久层

---
+++ sql/business.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member`
+(
+    id     BIGINT      NOT NULL COMMENT 'id',
+    mobile VARCHAR(11) NULL COMMENT '手机号',
+    PRIMARY KEY (id),
+    UNIQUE KEY `member_unique` (`mobile`)
+) engine=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '会员';

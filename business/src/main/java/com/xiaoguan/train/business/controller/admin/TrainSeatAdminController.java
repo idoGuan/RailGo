b731d56d957e86ca23baefd7a756fa738c3690b3
ZWHY <1760785369@qq.com>
2024-05-21 08:57:44 +0800

    增加火车座位表，生成持久层代码及后端代码

---
+++ business/src/main/java/com/xiaoguan/train/business/controller/admin/TrainSeatAdminController.java
@@ -0,0 +1,49 @@
+package com.xiaoguan.train.business.controller.admin;
+
+import com.xiaoguan.train.common.context.LoginMemberContext;
+import com.xiaoguan.train.common.resp.CommonResp;
+import com.xiaoguan.train.common.resp.PageResp;
+import com.xiaoguan.train.business.req.TrainSeatQueryReq;
+import com.xiaoguan.train.business.req.TrainSeatSaveReq;
+import com.xiaoguan.train.business.resp.TrainSeatQueryResp;
+import com.xiaoguan.train.business.service.TrainSeatService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Configuration;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * ClassName: TestController
+ * Package: com.xiaoguan.train.trainSeat.controller
+ * Description:
+ *
+ * @Author 小管不要跑
+ * @Create 2024/5/14 13:47
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/admin/train-seat")
+public class TrainSeatAdminController {
+
+    @Resource
+    private TrainSeatService trainSeatService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TrainSeatSaveReq req){
+        trainSeatService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TrainSeatQueryResp>> queryList(@Valid TrainSeatQueryReq req){
+        PageResp<TrainSeatQueryResp> trainSeatQueryRespList = trainSeatService.queryList(req);
+        return new CommonResp<>(trainSeatQueryRespList);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id){
+        trainSeatService.delete(id);
+        return new CommonResp<>();
+    }
+}

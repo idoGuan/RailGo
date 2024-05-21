899639102730de304aa975680045fcd6d5d6a4b5
ZWHY <1760785369@qq.com>
2024-05-21 14:54:46 +0800

    火车车次下拉选择功能开发，增加后端接口

--- business/src/main/java/com/xiaoguan/train/business/controller/admin/StationAdminController.java
+++ business/src/main/java/com/xiaoguan/train/business/controller/admin/StationAdminController.java
@@ -10,6 +10,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * ClassName: TestController
  * Package: com.xiaoguan.train.station.controller
@@ -42,5 +44,11 @@ public class StationAdminController {
     public CommonResp<Object> delete(@PathVariable Long id){
         stationService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<StationQueryResp>> queryAll(){
+        List<StationQueryResp> stationQueryRespList = stationService.queryAll();
+        return new CommonResp<>(stationQueryRespList);
     }
 }

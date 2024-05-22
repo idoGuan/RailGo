b89eca48d584495412b57046074f961e3634f6c6
ZWHY <1760785369@qq.com>
2024-05-22 17:10:56 +0800

    完善每日车站页面，增加日期和车次查询条件

--- business/src/main/java/com/xiaoguan/train/business/req/DailyTrainStationQueryReq.java
+++ business/src/main/java/com/xiaoguan/train/business/req/DailyTrainStationQueryReq.java
@@ -1,12 +1,39 @@
 package com.xiaoguan.train.business.req;
 
 import com.xiaoguan.train.common.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainStationQueryReq extends PageReq {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String trainCode;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainStationQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
+
 }
--- business/src/main/java/com/xiaoguan/train/business/service/DailyTrainStationService.java
+++ business/src/main/java/com/xiaoguan/train/business/service/DailyTrainStationService.java
@@ -44,8 +44,15 @@ public class DailyTrainStationService {
 
     public PageResp<DailyTrainStationQueryResp> queryList(DailyTrainStationQueryReq req) {
         DailyTrainStationExample dailyTrainStationExample = new DailyTrainStationExample();
-        dailyTrainStationExample.setOrderByClause("id desc");
+        dailyTrainStationExample.setOrderByClause("date desc, train_code asc, `index` asc");
         DailyTrainStationExample.Criteria criteria = dailyTrainStationExample.createCriteria();
+
+        if(ObjectUtil.isNotNull(req.getDate())){
+            criteria.andDateEqualTo(req.getDate());
+        }
+        if(ObjectUtil.isNotEmpty(req.getTrainCode())){
+            criteria.andTrainCodeEqualTo(req.getTrainCode());
+        }
 
         LOG.info("查询页码：{}", req.getPage());
         LOG.info("每页条数：{}", req.getSize());

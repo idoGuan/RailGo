8744a8a3f6802bc233fa9022fefc9d1b4ead8509
ZWHY <1760785369@qq.com>
2024-05-23 20:00:43 +0800

    增加确认订单表

---
+++ business/src/main/java/com/xiaoguan/train/business/controller/admin/ConfirmOrderAdminController.java
@@ -0,0 +1,49 @@
+package com.xiaoguan.train.business.controller.admin;
+
+import com.xiaoguan.train.common.context.LoginMemberContext;
+import com.xiaoguan.train.common.resp.CommonResp;
+import com.xiaoguan.train.common.resp.PageResp;
+import com.xiaoguan.train.business.req.ConfirmOrderQueryReq;
+import com.xiaoguan.train.business.req.ConfirmOrderSaveReq;
+import com.xiaoguan.train.business.resp.ConfirmOrderQueryResp;
+import com.xiaoguan.train.business.service.ConfirmOrderService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Configuration;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * ClassName: TestController
+ * Package: com.xiaoguan.train.confirmOrder.controller
+ * Description:
+ *
+ * @Author 小管不要跑
+ * @Create 2024/5/14 13:47
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/admin/confirm-order")
+public class ConfirmOrderAdminController {
+
+    @Resource
+    private ConfirmOrderService confirmOrderService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderSaveReq req){
+        confirmOrderService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<ConfirmOrderQueryResp>> queryList(@Valid ConfirmOrderQueryReq req){
+        PageResp<ConfirmOrderQueryResp> confirmOrderQueryRespList = confirmOrderService.queryList(req);
+        return new CommonResp<>(confirmOrderQueryRespList);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id){
+        confirmOrderService.delete(id);
+        return new CommonResp<>();
+    }
+}
---
+++ business/src/main/java/com/xiaoguan/train/business/service/ConfirmOrderService.java
@@ -0,0 +1,70 @@
+package com.xiaoguan.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.xiaoguan.train.common.resp.PageResp;
+import com.xiaoguan.train.common.util.SnowUtil;
+import com.xiaoguan.train.business.domain.ConfirmOrder;
+import com.xiaoguan.train.business.domain.ConfirmOrderExample;
+import com.xiaoguan.train.business.mapper.ConfirmOrderMapper;
+import com.xiaoguan.train.business.req.ConfirmOrderQueryReq;
+import com.xiaoguan.train.business.req.ConfirmOrderSaveReq;
+import com.xiaoguan.train.business.resp.ConfirmOrderQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConfirmOrderService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConfirmOrderService.class);
+
+    @Resource
+    private ConfirmOrderMapper confirmOrderMapper;
+
+    public void save(ConfirmOrderSaveReq req) {
+        DateTime now = DateTime.now();
+        ConfirmOrder confirmOrder = BeanUtil.copyProperties(req, ConfirmOrder.class);
+        if (ObjectUtil.isNull(confirmOrder.getId())) {
+            confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+            confirmOrder.setCreateTime(now);
+            confirmOrder.setUpdateTime(now);
+            confirmOrderMapper.insert(confirmOrder);
+        } else {
+            confirmOrder.setUpdateTime(now);
+            confirmOrderMapper.updateByPrimaryKey(confirmOrder);
+        }
+    }
+
+    public PageResp<ConfirmOrderQueryResp> queryList(ConfirmOrderQueryReq req) {
+        ConfirmOrderExample confirmOrderExample = new ConfirmOrderExample();
+        confirmOrderExample.setOrderByClause("id desc");
+        ConfirmOrderExample.Criteria criteria = confirmOrderExample.createCriteria();
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<ConfirmOrder> confirmOrderList = confirmOrderMapper.selectByExample(confirmOrderExample);
+
+        PageInfo<ConfirmOrder> pageInfo = new PageInfo<>(confirmOrderList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<ConfirmOrderQueryResp> list = BeanUtil.copyToList(confirmOrderList, ConfirmOrderQueryResp.class);
+
+        PageResp<ConfirmOrderQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        confirmOrderMapper.deleteByPrimaryKey(id);
+    }
+}

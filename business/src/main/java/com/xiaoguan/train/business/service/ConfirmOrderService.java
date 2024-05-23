ec4a8fb162c7425593ac9fb4ee4077e76e4513e2
ZWHY <1760785369@qq.com>
2024-05-23 20:47:22 +0800

    保存确认订单，状态初始

--- business/src/main/java/com/xiaoguan/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/xiaoguan/train/business/service/ConfirmOrderService.java
@@ -3,8 +3,11 @@ package com.xiaoguan.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.xiaoguan.train.business.enums.ConfirmOrderStatusEnum;
+import com.xiaoguan.train.common.context.LoginMemberContext;
 import com.xiaoguan.train.common.resp.PageResp;
 import com.xiaoguan.train.common.util.SnowUtil;
 import com.xiaoguan.train.business.domain.ConfirmOrder;
@@ -71,7 +74,21 @@ public class ConfirmOrderService {
     public void doConfirm(ConfirmOrderDoReq req) {
         //省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，ticket条数>0，同乘客同车次是否已经买过
 
-        //保存确认订单，状态初识
+        //保存确认订单，状态初始
+        DateTime now = DateTime.now();
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+        confirmOrderMapper.insert(confirmOrder);
 
         //查出余票记录，需要得到真实的库存
 

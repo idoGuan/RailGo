5ed685c0f6edcc9fa920732aa18dc61ec6d4763f
ZWHY <1760785369@qq.com>
2024-05-25 17:23:20 +0800

    演示Mybatis的二级缓存

--- business/src/main/java/com/xiaoguan/train/business/domain/Train.java
+++ business/src/main/java/com/xiaoguan/train/business/domain/Train.java
@@ -1,8 +1,9 @@
 package com.xiaoguan.train.business.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Train {
+public class Train implements Serializable {
     private Long id;
 
     private String code;

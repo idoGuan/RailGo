23fe84e84b3605f9f81a147dda1a24767218ae44
ZWHY <1760785369@qq.com>
2024-05-25 17:24:02 +0800

    去掉Mybatis的二级缓存

--- business/src/main/java/com/xiaoguan/train/business/domain/Train.java
+++ business/src/main/java/com/xiaoguan/train/business/domain/Train.java
@@ -1,9 +1,8 @@
 package com.xiaoguan.train.business.domain;
 
-import java.io.Serializable;
 import java.util.Date;
 
-public class Train implements Serializable {
+public class Train{
     private Long id;
 
     private String code;

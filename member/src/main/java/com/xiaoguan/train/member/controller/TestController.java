4977889af5514d2b14cf20efb41e8d1a2765e094
ZWHY <1760785369@qq.com>
2024-05-25 10:12:22 +0800

    读取配置文件的配置

--- member/src/main/java/com/xiaoguan/train/member/controller/TestController.java
+++ member/src/main/java/com/xiaoguan/train/member/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.xiaoguan.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello world!";
+        return "hello " + testNacos;
     }
 }

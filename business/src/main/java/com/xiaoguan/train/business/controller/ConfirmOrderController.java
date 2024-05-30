ed05b949734ca6ecf46b5d9825fd4a93b43b34b6
ZWHY <1760785369@qq.com>
2024-05-30 09:16:08 +0800

    购票接口，增加验证码校验

--- business/src/main/java/com/xiaoguan/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/xiaoguan/train/business/controller/ConfirmOrderController.java
@@ -10,6 +10,9 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +33,9 @@ public class ConfirmOrderController {
 
     public static final Logger LOG = LoggerFactory.getLogger(ConfirmOrderController.class);
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Resource
     private ConfirmOrderService confirmOrderService;
 
@@ -37,6 +43,21 @@ public class ConfirmOrderController {
     @SentinelResource(value = "confirmOrderDo", blockHandler = "doConfirmBlock")
     @PostMapping("/do")
     public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req){
+        // 图形验证码校验
+        String imageCodeToken = req.getImageCodeToken();
+        String imageCode = req.getImageCode();
+        String imageCodeRedis = redisTemplate.opsForValue().get(imageCodeToken);
+        LOG.info("从redis中获取到的验证码：{}", imageCodeRedis);
+        if (ObjectUtils.isEmpty(imageCodeRedis)) {
+            return new CommonResp<>(false, "验证码已过期", null);
+        }
+        // 验证码校验，大小写忽略，提升体验，比如Oo Vv Ww容易混
+        if (!imageCodeRedis.equalsIgnoreCase(imageCode)) {
+            return new CommonResp<>(false, "验证码不正确", null);
+        } else {
+            // 验证通过后，移除验证码
+            redisTemplate.delete(imageCodeToken);
+        }
         confirmOrderService.doConfirm(req);
         return new CommonResp<>();
     }

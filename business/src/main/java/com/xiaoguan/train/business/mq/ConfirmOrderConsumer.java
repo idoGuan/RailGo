a015ccf0950cb288cadba79a6cfecb4244e40631
ZWHY <1760785369@qq.com>
2024-05-31 08:31:56 +0800

    实现RocketMQ接收

---
+++ business/src/main/java/com/xiaoguan/train/business/mq/ConfirmOrderConsumer.java
@@ -0,0 +1,21 @@
+package com.xiaoguan.train.business.mq;
+
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RocketMQMessageListener(consumerGroup = "default", topic = "CONFIRM_ORDER")
+public class ConfirmOrderConsumer implements RocketMQListener<MessageExt> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConfirmOrderConsumer.class);
+
+    @Override
+    public void onMessage(MessageExt messageExt) {
+        byte[] body = messageExt.getBody();
+        LOG.info("ROCKETMQ收到消息：{}", new String(body));
+    }
+}
